9e33b57db60a2012db14ff0d15bd188af7658fd9
Ivan Meireles Da Silva <isilva@acesso.com>
2020-05-24 14:41:52 -0300

    criando elasticConfig

---
+++ configurations/elastic.ts
@@ -0,0 +1,17 @@
+import { LogsElastic } from "../repositories/elastic/request.log.elastic";
+
+export class ElasticConfig {
+
+    init(): Promise<any> {
+
+        return new Promise<any>((resolver, reject) => {
+            try {
+                LogsElastic.Create();
+                resolver();
+            } catch (err) {
+                reject();
+            }
+        });
+    }
+
+}
